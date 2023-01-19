cf90e55a28f378281718f168cda704000fb1c445
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-01-19 15:14:51 +0700

    add item product

---
+++ app/src/main/java/com/veroanggra/composesubmission/catalog/data/Product.kt
@@ -0,0 +1,10 @@
+package com.veroanggra.composesubmission.catalog.data
+
+data class Product(
+    val idProduct: Int,
+    val nameProduct: String,
+    val categoryProduct: String,
+    val priceProduct: String,
+    val isFavorite: Boolean,
+    val imageProduct: String
+)
---
+++ app/src/main/java/com/veroanggra/composesubmission/catalog/data/ProductData.kt
@@ -0,0 +1,12 @@
+package com.veroanggra.composesubmission.catalog.data
+
+object ProductData {
+    val product = listOf(
+        Product(idProduct = 1, nameProduct = "Sofa Empuk", categoryProduct = "Chair", priceProduct = "$700", isFavorite = false, imageProduct = "R.drawable.img_top_list"),
+        Product(idProduct = 2, nameProduct = "Sofa Empuk", categoryProduct = "Chair", priceProduct = "$500", isFavorite = false, imageProduct = "R.drawable.img_top_list"),
+        Product(idProduct = 3, nameProduct = "Sofa Empuk", categoryProduct = "Chair", priceProduct = "$400", isFavorite = false, imageProduct = "R.drawable.img_top_list"),
+        Product(idProduct = 4, nameProduct = "Sofa Empuk", categoryProduct = "Chair", priceProduct = "$230", isFavorite = false, imageProduct = "R.drawable.img_top_list"),
+        Product(idProduct = 5, nameProduct = "Sofa Empuk", categoryProduct = "Chair", priceProduct = "$730", isFavorite = false, imageProduct = "R.drawable.img_top_list"),
+        Product(idProduct = 6, nameProduct = "Sofa Empuk", categoryProduct = "Chair", priceProduct = "$930", isFavorite = false, imageProduct = "R.drawable.img_top_list")
+        )
+}
