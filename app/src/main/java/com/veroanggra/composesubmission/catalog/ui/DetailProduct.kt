0bd483704a5768ff389f4fbf0430ea2bc106dff2
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-01-24 21:24:53 +0700

    modify detail page

--- app/src/main/java/com/veroanggra/composesubmission/catalog/ui/DetailProduct.kt
+++ app/src/main/java/com/veroanggra/composesubmission/catalog/ui/DetailProduct.kt
@@ -28,6 +28,8 @@ import com.veroanggra.composesubmission.R
 import com.veroanggra.composesubmission.catalog.data.Product
 import com.veroanggra.composesubmission.catalog.data.ProductRepo
 import com.veroanggra.composesubmission.ui.theme.DarkCream
+import com.veroanggra.composesubmission.ui.theme.GreenPickerColor
+import com.veroanggra.composesubmission.ui.theme.YellowPickerColor
 
 @Composable
 fun DetailProduct(idProduct: Int) {
@@ -35,7 +37,8 @@ fun DetailProduct(idProduct: Int) {
         ProductRepo.getProduct(idProduct = idProduct)
     }
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        val (imageProduct, titleProduct, subTitleProduct, descProduct, priceProduct) = createRefs()
+        val (imageProduct, titleProduct, subTitleProduct, descProduct, priceProduct, space, colorTitle, colorSwatch1, colorSwatch2, colorSwatchShade1, colorSwatchShade2, space1, space2, space3, space4) = createRefs()
+        val (space5, space6, colorSwatch3, colorSwatchShade3) = createRefs()
         Card(
             modifier = Modifier
                 .height(380.dp)
@@ -50,10 +53,9 @@ fun DetailProduct(idProduct: Int) {
         ) {
             ConstraintLayout(modifier = Modifier.fillMaxSize()) {
                 val (imgProduct, backButton, favButton) = createRefs()
-                Image(
-                    painter = painterResource(
-                        id = product?.imageProduct ?: R.drawable.img_top_list
-                    ),
+                Image(painter = painterResource(
+                    id = product?.imageProduct ?: R.drawable.img_top_list
+                ),
                     contentDescription = null,
                     modifier = Modifier
                         .fillMaxWidth()
@@ -64,18 +66,16 @@ fun DetailProduct(idProduct: Int) {
                             end.linkTo(parent.end)
                             top.linkTo(backButton.bottom)
                             bottom.linkTo(parent.bottom)
-                        }
-                )
+                        })
                 Box(
                     Modifier
-                        .padding(end = 20.dp, top = 30.dp)
+                        .padding(end = 20.dp, top = 20.dp)
                         .background(color = Color.LightGray, shape = CircleShape)
                         .clip(CircleShape)
                         .constrainAs(favButton) {
                             end.linkTo(parent.end)
                             top.linkTo(parent.top)
-                        }
-                ) {
+                        }) {
                     Icon(
                         Icons.Outlined.Favorite,
                         contentDescription = null,
@@ -87,18 +87,16 @@ fun DetailProduct(idProduct: Int) {
                             .align(alignment = Alignment.Center)
                     )
                 }
-                Box(
-                    modifier = Modifier
-                        .padding(start = 30.dp, top = 20.dp)
-                        .background(color = Color.LightGray, shape = CircleShape)
-                        .clip(
-                            CircleShape
-                        )
-                        .constrainAs(backButton) {
-                            start.linkTo(parent.start)
-                            top.linkTo(parent.top)
-                        }
-                ) {
+                Box(modifier = Modifier
+                    .padding(start = 30.dp, top = 20.dp)
+                    .background(color = Color.LightGray, shape = CircleShape)
+                    .clip(
+                        CircleShape
+                    )
+                    .constrainAs(backButton) {
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                    }) {
                     Icon(
                         Icons.Outlined.ArrowBack,
                         contentDescription = null,
@@ -119,7 +117,7 @@ fun DetailProduct(idProduct: Int) {
                 text = it.nameProduct,
                 fontSize = 24.sp,
                 modifier = Modifier
-                    .padding(start = 20.dp, end = 20.dp, top = 20.dp)
+                    .padding(start = 30.dp, end = 30.dp, top = 20.dp)
                     .constrainAs(titleProduct) {
                         start.linkTo(parent.start)
                         top.linkTo(imageProduct.bottom)
@@ -132,7 +130,7 @@ fun DetailProduct(idProduct: Int) {
                 text = it.subTitle,
                 fontSize = 16.sp,
                 modifier = Modifier
-                    .padding(start = 20.dp, end = 20.dp, top = 5.dp)
+                    .padding(start = 30.dp, end = 30.dp, top = 5.dp)
                     .constrainAs(subTitleProduct) {
                         start.linkTo(parent.start)
                         top.linkTo(titleProduct.bottom)
@@ -141,18 +139,146 @@ fun DetailProduct(idProduct: Int) {
             )
         }
         product?.let {
-            Text(
-                text = "Rp ${it.priceProduct}",
+            Text(text = "Rp ${it.priceProduct}",
                 fontSize = 20.sp,
                 style = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
                 modifier = Modifier
-                    .padding(end = 20.dp)
+                    .padding(end = 30.dp)
                     .constrainAs(priceProduct) {
                         end.linkTo(parent.end)
                         top.linkTo(titleProduct.top)
                         bottom.linkTo(subTitleProduct.bottom)
-                    }
-            )
+                    })
         }
+        product?.let {
+            Text(text = it.desc,
+                fontSize = 14.sp,
+                style = TextStyle(color = Color.Gray),
+                modifier = Modifier
+                    .padding(start = 30.dp, top = 20.dp, end = 30.dp)
+                    .constrainAs(descProduct) {
+                        start.linkTo(parent.start)
+                        top.linkTo(subTitleProduct.bottom)
+                        end.linkTo(parent.end)
+                    })
+        }
+        Spacer(modifier = Modifier
+            .height(40.dp)
+            .constrainAs(space) {
+                top.linkTo(descProduct.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
+        Text(text = "Color",
+            style = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+            fontSize = 20.sp,
+            modifier = Modifier
+                .constrainAs(colorTitle) {
+                    start.linkTo(parent.start)
+                    top.linkTo(space.bottom)
+                }
+                .padding(start = 30.dp))
+        Spacer(modifier = Modifier
+            .width(50.dp)
+            .constrainAs(space1) {
+                start.linkTo(colorTitle.end)
+                top.linkTo(colorTitle.top)
+                bottom.linkTo(colorTitle.bottom)
+            })
+        Spacer(modifier = Modifier
+            .width(48.dp)
+            .constrainAs(space2) {
+                start.linkTo(colorTitle.end)
+                top.linkTo(colorTitle.top)
+                bottom.linkTo(colorTitle.bottom)
+            })
+        Box(modifier = Modifier
+            .height(34.dp)
+            .width(34.dp)
+            .background(color = DarkCream, shape = CircleShape)
+            .clip(CircleShape)
+            .constrainAs(colorSwatchShade1) {
+                start.linkTo(space2.end)
+                top.linkTo(space2.top)
+                bottom.linkTo(space2.bottom)
+            })
+        Box(modifier = Modifier
+            .height(30.dp)
+            .width(30.dp)
+            .background(color = Color.LightGray, shape = CircleShape)
+            .clip(CircleShape)
+            .constrainAs(colorSwatch1) {
+                start.linkTo(space1.end)
+                top.linkTo(space1.top)
+                bottom.linkTo(space1.bottom)
+            })
+        Spacer(modifier = Modifier
+            .width(10.dp)
+            .constrainAs(space4) {
+                start.linkTo(colorSwatch1.end)
+                top.linkTo(colorSwatch1.top)
+                bottom.linkTo(colorSwatch1.bottom)
+            })
+        Spacer(modifier = Modifier
+            .width(8.dp)
+            .constrainAs(space3) {
+                start.linkTo(colorSwatchShade1.end)
+                top.linkTo(colorSwatchShade1.top)
+                bottom.linkTo(colorSwatchShade1.bottom)
+            })
+        Box(modifier = Modifier
+            .height(34.dp)
+            .width(34.dp)
+            .background(color = DarkCream, shape = CircleShape)
+            .clip(CircleShape)
+            .constrainAs(colorSwatchShade2) {
+                start.linkTo(space3.end)
+                top.linkTo(space3.top)
+                bottom.linkTo(space3.bottom)
+            })
+        Box(modifier = Modifier
+            .height(30.dp)
+            .width(30.dp)
+            .background(color = GreenPickerColor, shape = CircleShape)
+            .clip(CircleShape)
+            .constrainAs(colorSwatch2) {
+                start.linkTo(space4.end)
+                top.linkTo(space4.top)
+                bottom.linkTo(space4.bottom)
+            })
+        Spacer(modifier = Modifier
+            .width(10.dp)
+            .constrainAs(space5) {
+                start.linkTo(colorSwatch2.end)
+                top.linkTo(colorSwatch2.top)
+                bottom.linkTo(colorSwatch2.bottom)
+            })
+        Spacer(modifier = Modifier
+            .width(8.dp)
+            .constrainAs(space6) {
+                start.linkTo(colorSwatchShade2.end)
+                top.linkTo(colorSwatchShade2.top)
+                bottom.linkTo(colorSwatchShade2.bottom)
+            })
+        Box(modifier = Modifier
+            .height(34.dp)
+            .width(34.dp)
+            .background(color = DarkCream, shape = CircleShape)
+            .clip(CircleShape)
+            .constrainAs(colorSwatchShade3) {
+                start.linkTo(space6.end)
+                top.linkTo(space6.top)
+                bottom.linkTo(space6.bottom)
+            })
+        Box(modifier = Modifier
+            .height(30.dp)
+            .width(30.dp)
+            .background(color = YellowPickerColor, shape = CircleShape)
+            .clip(CircleShape)
+            .constrainAs(colorSwatch3) {
+                start.linkTo(space5.end)
+                top.linkTo(space5.top)
+                bottom.linkTo(space5.bottom)
+            })
     }
 }
